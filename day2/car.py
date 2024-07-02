0ffb51d631688b1bfa3e1ce7901125af4acc10de
baozi <caz69012@gmail.com>
2024-07-02 15:11:15 +0800

    7.2 third commit

---
+++ day2/car.py
@@ -0,0 +1,22 @@
+# 车辆识别
+# encoding:utf-8
+
+import requests
+import base64
+
+'''
+车型识别
+'''
+
+request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/car"
+# 二进制方式打开图片文件
+f = open('/Users/baozi/summer_training/day2/demo3.jpg', 'rb')
+img = base64.b64encode(f.read())
+
+params = {"image":img,"top_num":5}
+access_token = '24.72c5e2b23aa07e134c733c0d081eeaaa.2592000.1722478097.282335-89934902'
+request_url = request_url + "?access_token=" + access_token
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+response = requests.post(request_url, data=params, headers=headers)
+if response:
+    print (response.json())
---
+++ day2/web_image.py
@@ -0,0 +1,22 @@
+#网络文字识别
+# encoding:utf-8
+
+import requests
+import base64
+
+'''
+网络图片文字识别
+'''
+
+request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/webimage"
+# 二进制方式打开图片文件
+f = open('/Users/baozi/summer_training/day2/demo4.jpg', 'rb')
+img = base64.b64encode(f.read())
+
+params = {"image":img}
+access_token = '24.72c5e2b23aa07e134c733c0d081eeaaa.2592000.1722478097.282335-89934902'
+request_url = request_url + "?access_token=" + access_token
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+response = requests.post(request_url, data=params, headers=headers)
+if response:
+    print (response.json())
