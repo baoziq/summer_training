ef96f35c0dade25e26c179e069c77eab7d036711
baozi <caz69012@gmail.com>
2024-07-03 16:27:25 +0800

    7.3 second commit

--- day3/app/maindialog.py
+++ day3/app/maindialog.py
@@ -1,8 +1,32 @@
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QDialog
 from app.mdui import Ui_Dialog
+import cv2 as cv
+from ai.haha import DistortEffect
 class MainDialog(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.e = DistortEffect()
+        self.img = cv.imread('data/1.jpg')
+        self.showimg()
+
+    def showimg(self):
+        # opencv图片 =>QImage=>QPixmap=>按比例缩放==>QT labels显示
+        h, w, c = self.img.shape
+        img_bytes= self.img.tobytes()
+
+        imgae = QImage(img_bytes, w, h, w * c, QImage.Format_BGR888)
+        pix = QPixmap.fromImage(imgae)
+        # 自动缩放
+        width = self.ui.label_2.width()
+        height = self.ui.label_2.height()
+        scale_pix = pix.scaled(width, height, Qt.KeepAspectRatio)
+        self.ui.label_2.setPixmap(scale_pix)
+
+    def big(self):
+        self.img = self.e.MinFrame(self.img)
+        self.showimg()
 
--- day3/app/mdui.py
+++ day3/app/mdui.py
@@ -14,23 +14,33 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(835, 530)
+        Dialog.resize(684, 587)
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(190, 80, 431, 81))
+        self.label.setGeometry(QtCore.QRect(110, 20, 431, 81))
         font = QtGui.QFont()
         font.setFamily("Arial")
         self.label.setFont(font)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(350, 180, 93, 28))
+        self.pushButton.setGeometry(QtCore.QRect(100, 510, 481, 51))
         self.pushButton.setObjectName("pushButton")
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setGeometry(QtCore.QRect(190, 110, 291, 371))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 271, 351))
+        self.label_2.setObjectName("label_2")
 
         self.retranslateUi(Dialog)
+        self.pushButton.clicked.connect(Dialog.big) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "计算机专业"))
-        self.pushButton.setText(_translate("Dialog", "选择"))
+        self.label.setText(_translate("Dialog", "哈哈镜效果"))
+        self.pushButton.setText(_translate("Dialog", "哈哈镜"))
+        self.label_2.setText(_translate("Dialog", "TextLabel"))
